71cf90c1c5fdb5d620a3398421e00d6a6b7210f3
sumanshrestha2009 <97793834+sumanshrestha2009@users.noreply.github.com>
2025-02-19 20:12:20 +0545

    Update sumanshrestha.js

--- sumanshrestha.js
+++ sumanshrestha.js
@@ -15,6 +15,21 @@
   })
 
 
+    //Tab to top
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1){  
+        $('.scroll-top-wrapper').addClass("show");
+        }
+    else{
+        $('.scroll-top-wrapper').removeClass("show");
+        }
+    });
+    $(".scroll-top-wrapper").on("click", function() {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+
 
   $('#mixedSlider').multislider({
       duration: 750,
